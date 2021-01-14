ce84a0a2e6296f0fca74bc9ce46d63ef392445a7
omeednp <omeed.naraghipour@gmail.com>
2021-01-14 14:15:33 -0800

    Very cool

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,31 @@
-# Add your code here
+require 'pry'
+class Dog
+
+    attr_reader :name
+
+    def initialize (name)
+        @name = name
+        save
+    end
+
+    @@all = []
+
+    def self.all
+        @@all
+    end
+    
+    def self.clear_all
+        @@all = []
+    end
+
+    def self.print_all
+        @@all.map do |dog|
+            puts dog.name
+        end
+    end
+
+    def save
+        @@all << self
+    end
+
+end
